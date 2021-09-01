052e8fc8bc0a85aaa290992e4325778d9a245012
Alexis Rosin <rosinalexis@yahoo.fr>
2021-09-01 18:02:45 +0200

    ajout de la partie de l'equipe à une personne

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Equipe;
 use App\Entity\Personne;
+use App\Repository\EquipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,17 +16,26 @@ class PersonneController extends AbstractController
     /**
      * @Route("/personne/ajouter", name="app_personne_ajouter", methods={"POST"})
      */
-    public function ajouter(Request $request, EntityManagerInterface $em): Response
+    public function ajouter(Request $request, EntityManagerInterface $em, EquipeRepository $equipeRepo): Response
     {
-        dd($request->request);
+        
         $nom = $request->request->get('personne_nom');
         $prenom = $request->request->get('personne_prenom');
+        $equipes_tab = $request->request->get('personne_equipes');
+
 
         if($prenom && $nom){
             $personne = new Personne; 
             $personne->setNom($nom); 
             $personne->setPrenom($prenom); 
 
+            if($equipes_tab){
+                foreach ((array) $equipes_tab as $equipe_id) {
+                    $equipe = $equipeRepo->findOneBy(['id'=>$equipe_id]);
+                    $personne->addEquipe($equipe); 
+                }
+            }
+        
             $em->persist($personne); 
             $em->flush();
         }
